0282524c66b788160c0f20f58877b9b9c45e1699
DONTBLAMEME777 <w2045712@westminster.ac.uk>
2024-05-12 14:07:36 +0100

    Adds comments on enemy scripts

--- Assets/Scripts/World/Enemies/Pendulum.cs
+++ Assets/Scripts/World/Enemies/Pendulum.cs
@@ -4,28 +4,29 @@ using UnityEngine;
 
 public class Pendulum : MonoBehaviour
 {
-    public float speed;
+    public float speed;//impulse strength
     private Rigidbody2D rigidbody2D;
     public float leftAngle;
     public float rightAngle;
-    bool movingClockwise;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
-        rigidbody2D.angularVelocity = speed*10;
+        rigidbody2D.angularVelocity = speed*10;//gives initial impulse anti clockwise for it to reach the rightAngle
     }
 
     // Update is called once per frame
     void Update()
     {
+        //if pendulum exceeds the right angle limit
         if (transform.rotation.z > rightAngle)
         {
-            rigidbody2D.angularVelocity = -1*speed;
+            rigidbody2D.angularVelocity = -1*speed;//makes pendulum swing clockwise
         }
+        //if pendulum exceeds the left angle limit
         if (transform.rotation.z < leftAngle)
         {
-            rigidbody2D.angularVelocity = speed;
+            rigidbody2D.angularVelocity = speed;//makes pendulum swing anti clockwise
         }
     }
 
