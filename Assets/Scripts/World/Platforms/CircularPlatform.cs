dbe3d807be42c5844ff309b4ce64c65ff035a648
moiezl <themasihmob@gmail.com>
2024-05-13 16:33:26 +0100

    Update CircularPlatform.cs

--- Assets/Scripts/World/Platforms/CircularPlatform.cs
+++ Assets/Scripts/World/Platforms/CircularPlatform.cs
@@ -8,25 +8,29 @@ public class CircularPlatform : MonoBehaviour
     [SerializeField] private float amplitude;
     [SerializeField] private bool flip;
     private Rigidbody2D rb;
+    private Vector3 initialPosition; // Store the initial position
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        initialPosition = transform.position; // Initialize the initial position
     }
 
     void FixedUpdate()
     {
+        float x, y;
+
         if (!flip)
         {
-            float x = Mathf.Cos(Time.time * frequency) * amplitude;
-            float y = Mathf.Sin(Time.time * frequency) * amplitude;
-            rb.MovePosition(new Vector3(x, y, transform.position.z));
+            x = Mathf.Cos(Time.time * frequency) * amplitude;
+            y = Mathf.Sin(Time.time * frequency) * amplitude;
         }
         else
         {
-            float x = Mathf.Cos(Time.time * -frequency) * amplitude;
-            float y = Mathf.Sin(Time.time * -frequency) * amplitude;
-            rb.MovePosition(new Vector3(x, y, transform.position.z));
+            x = Mathf.Cos(Time.time * -frequency) * amplitude;
+            y = Mathf.Sin(Time.time * -frequency) * amplitude;
         }
+
+        rb.MovePosition(new Vector3(x, y, transform.position.z) + initialPosition); // Add initial position
     }
 }
